6d3532da699674b87c27a3618317292b50f25985
davit2kutelia <datakutelia8@gmail.com>
2024-08-09 20:35:55 +0400

    second commit

--- day-00/homework/leasson 1 .py
+++ day-00/homework/leasson 1 .py
@@ -1,72 +1,82 @@
-from turtle import *
-#we want to paint a house
-#step 1: draw a aquare
+i = 1
+while i <= 30:
+    if i % 2 == 0:
+        print(f'{i} - odd')
+    else:
+        print(f'{i} -even ')
+    i += 1
 
-
-
-width(7)
-color("yellow")
-forward(200)
-left(90)
-forward(200)
-left(90)
-forward(200)
-left(90)
-forward(200)
-left(90)
-#end of square
-
-#drawing door
-
-forward(70)
-color("red")
-begin_fill()
-left(90)
-forward(120)    #heigh of the door
-right(90)
-forward(60)
-right(90)
-forward(120)
-end_fill()
-
-penup()
-goto(200,200)
-pendown()
-color("red")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
-end_fill()
-
-penup()
-goto(20,180)
-pendown()
-left(30)
-forward(40)
-left(90)
-forward(40)
-left(90)
-forward(40)
-left(90)
-forward(40)
-penup()
-goto(180,180)
-pendown()
-forward(40)
-left(90)
-forward(40)
-left(90)
-forward(40)
-left(90)
-forward(40)
-
-
-
-
-
-
-
-
-exitonclick()
+.parent {
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        
+        
+    }
+    
+    body {
+        background-color: rgb(0, 0, 0);
+        display: flex;
+        justify-content: start;
+        align-items: center;
+    }
+    
+    .parent {
+        width: 2000px;
+        height: 800px;
+        border-radius: 40px;
+        box-shadow: 0 0 20px rgb(206, 206, 206);
+        background-color: white;
+        overflow: hidden;
+        display: flex;
+    }
+    
+    .image-div, .form-div {
+        width: 50%;
+    }
+    
+    .image-div {
+        background-image: url(istockphoto-1987435635-1024x1024.webp);
+        background-size: cover;
+        background-position: bottom;
+        justify-content: end;
+    }
+    
+    .form-div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 70px;
+    }
+    
+    .form-div form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
+    
+    input {
+        width: 100%;
+        padding: 20px 10px;
+        border: none;
+        border-radius: 17px;
+        outline: none;
+        background-color: rgb(240, 240, 240);
+    }
+    
+    button {
+        width: 100%;
+        padding: 20px 10px;
+        border-radius: 17px;
+        background-color: rgb(41, 98, 255);
+        border: 1px solid rgb(41, 98, 255);
+        color: aliceblue;
+        transition: 0.25s;
+        cursor: pointer;
+    }
+    
+}
